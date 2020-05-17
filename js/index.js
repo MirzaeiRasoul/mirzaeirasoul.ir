5e80b2f22b9ed8a4eac3a640533ff0affb7ff00b
MirzaeiRasoul <mirzaei.rasoul172@gmail.com>
2020-05-17 05:39:13 +0430

    Remove SplitText Plugin

--- js/index.js
+++ js/index.js
@@ -8,19 +8,37 @@
         $('.read-more, .header-section-social, .header-section-scroll-down, .header-section-image').removeClass("hide");
     } else {
         localStorage.setItem("first-load", "true");
-        var textSplit = SplitText.create(".text-animation", {type: "chars, words"});
+
         var loadTimeline = gsap.timeline();
         var spotlightTimeline = gsap.timeline();
         spotlightTimeline.fromTo(".header-section-image.light", {top: -150, autoAlpha: 0}, {top: 0, autoAlpha: 1, duration: 2});
-        loadTimeline.fromTo(
-            textSplit.chars,
-            {top: -100, autoAlpha: 0},
-            {top: 0, autoAlpha: 1, duration: 1.5, stagger: 0.1, ease: Back.easeOut,
-                onComplete: function() {
-                    spotlightTimeline.fromTo(".header-section-image.shine", {autoAlpha: 0}, {autoAlpha: 1, duration: 1});
+        var textItems = document.body.querySelectorAll(".text-animation");
+        textItems.forEach(function(item, index) {
+            var textSplit = acAnimated.Plugins.SplitText(item, {words: 1, chars: 1, spacing: 10});
+            loadTimeline.fromTo(
+                textSplit.chars,
+                {top: -100, autoAlpha: 0},
+                {top: 0, autoAlpha: 1, duration: 1, stagger: 0.1, ease: Back.easeOut,
+                    onComplete: function() {
+                        spotlightTimeline.fromTo(".header-section-image.shine", {autoAlpha: 0}, {autoAlpha: 1, duration: 1});
+                    }
                 }
-            }
-        );
+            );
+        });
+
+        // var textSplit = SplitText.create(".text-animation", {type: "chars, words"});
+        // var loadTimeline = gsap.timeline();
+        // var spotlightTimeline = gsap.timeline();
+        // spotlightTimeline.fromTo(".header-section-image.light", {top: -150, autoAlpha: 0}, {top: 0, autoAlpha: 1, duration: 2});
+        // loadTimeline.fromTo(
+        //     textSplit.chars,
+        //     {top: -100, autoAlpha: 0},
+        //     {top: 0, autoAlpha: 1, duration: 1.5, stagger: 0.1, ease: Back.easeOut,
+        //         onComplete: function() {
+        //             spotlightTimeline.fromTo(".header-section-image.shine", {autoAlpha: 0}, {autoAlpha: 1, duration: 1});
+        //         }
+        //     }
+        // );
         loadTimeline.to(".read-more", {autoAlpha: 1, duration: 0.5});
         loadTimeline.to(".header-section-social", {autoAlpha: 1, delay: 0.5, duration: 0.25});
         loadTimeline.to(".header-section-scroll-down", {
